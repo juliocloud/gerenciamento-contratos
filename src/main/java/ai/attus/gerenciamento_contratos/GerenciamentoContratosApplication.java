61f273b7754f3576f63864524597b616373f60e6
juliocloud <juliocesarjuriolli@gmail.com>
2024-12-17 19:45:07 -0300

    chore: application boilerplate

---
+++ src/main/java/ai/attus/gerenciamento_contratos/GerenciamentoContratosApplication.java
@@ -0,0 +1,13 @@
+package ai.attus.gerenciamento_contratos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoContratosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoContratosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/attus/gerenciamento_contratos/GerenciamentoContratosApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.attus.gerenciamento_contratos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoContratosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
