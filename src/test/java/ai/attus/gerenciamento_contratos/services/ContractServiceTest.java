7e2fe6fa8ea6b5ddf87a7b4ce5bdb848f23438f2
juliocloud <juliocesarjuriolli@gmail.com>
2024-12-17 20:29:37 -0300

    tests: added junit integration

---
+++ src/test/java/ai/attus/gerenciamento_contratos/services/ContractServiceTest.java
@@ -0,0 +1,13 @@
+package ai.attus.gerenciamento_contratos.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ContractServiceTest {
+    @Test
+    @DisplayName("Should be Hello World!")
+    void shouldReturnHelloWorld(){
+        assertEquals("Hello World", "Hello World");
+    }
+}
