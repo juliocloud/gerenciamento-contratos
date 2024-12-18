f75d9f233d31fcb0a2f941313c152fb45b591487
juliocloud <juliocesarjuriolli@gmail.com>
2024-12-18 15:20:41 -0300

    feat: new validations for contract

--- src/main/java/ai/attus/gerenciamento_contratos/models/Contract.java
+++ src/main/java/ai/attus/gerenciamento_contratos/models/Contract.java
@@ -4,49 +4,33 @@ import ai.attus.gerenciamento_contratos.enums.ContractStatus;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GeneratorType;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Entity
 @Table(name = "contracts")
 public class Contract {
 
-    @Id
-    @Column(name = "contract_number", unique = true)
-    private String number;
-
-
-    @Column(name = "creation_date")
-    private LocalDateTime creationDate;
-
-    @Column(name = "description")
-    private String description;
-
-    @Column(name = "status")
-    @Enumerated(EnumType.STRING)
-    private ContractStatus status;
-
-    @Override
-    public String toString() {
-        return "Contract{" +
-                "number='" + number + '\'' +
-                ", creationDate=" + creationDate +
-                ", description='" + description + '\'' +
-                ", status=" + status +
-                '}';
-    }
-
-    @Version
-    private int version;
-
     public Contract(){
 
     }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(ContractStatus status) {
+        this.status = status;
+    }
+
 
     @JsonCreator
     public Contract(
@@ -68,6 +52,48 @@ public class Contract {
         this.creationDate = creationDate;
         this.description = description;
         this.status = status;
+    }
+
+    @Id
+    @Column(name = "contract_number", unique = true)
+    @NotNull(message = "Mandatory field")
+    private String number;
+
+    @Column(name = "creation_date")
+    @Past(message = "Cannot be a future date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "description")
+    @NotNull(message = "Mandatory field")
+    private String description;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private ContractStatus status;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+
+
+    @Version
+    private int version;
+
+
+
+    @Override
+    public String toString() {
+        return "Contract{" +
+                "number='" + number + '\'' +
+                ", creationDate=" + creationDate +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
     }
 //@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     //private List<Party> parties;
