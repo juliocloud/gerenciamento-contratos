28f3e45d47ef0eb51178b891e389a58acde9ca97
juliocloud <juliocesarjuriolli@gmail.com>
2024-12-18 11:58:31 -0300

    test: configuring webmvc

--- src/test/java/ai/attus/gerenciamento_contratos/controllers/EventControllerTest.java
+++ src/test/java/ai/attus/gerenciamento_contratos/controllers/EventControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-@WebMvcTest
 public class EventControllerTest {
     @Test
     @DisplayName("Should register event via API successfully")
