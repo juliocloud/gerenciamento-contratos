9615aee11ca1ae8b7655cea725c278b0beea2df4
juliocloud <juliocesarjuriolli@gmail.com>
2024-12-18 11:56:13 -0300

    chore: database configuration

---
+++ src/main/java/ai/attus/gerenciamento_contratos/config/DatabaseConfiguration.java
@@ -0,0 +1,40 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String dbDriverClassName;
+
+    @Value("${spring.datasource.hikari.maximum-pool-size}")
+    private int maxPoolSize;
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(dbUrl);
+        hikariConfig.setUsername(dbUsername);
+        hikariConfig.setPassword(dbPassword);
+        hikariConfig.setDriverClassName(dbDriverClassName);
+        hikariConfig.setMaximumPoolSize(maxPoolSize);
+
+        return new HikariDataSource(hikariConfig);
+    }
+}
