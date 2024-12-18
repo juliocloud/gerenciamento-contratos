19a2f03d8333f07341dc43b67b2ce3952c7213b5
juliocloud <juliocesarjuriolli@gmail.com>
2024-12-18 19:50:27 -0300

    chore: defining event and party schemas

--- src/main/java/ai/attus/gerenciamento_contratos/models/Event.java
+++ src/main/java/ai/attus/gerenciamento_contratos/models/Event.java
@@ -1,15 +1,46 @@
 package ai.attus.gerenciamento_contratos.models;
 
 import ai.attus.gerenciamento_contratos.enums.EventType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
-//@Entity
+@Data
+@Entity
+@Table(name = "events")
 public class Event {
 
-    //@Id
+    public Event() {
+    }
+
+    public Event(String id, EventType type, LocalDateTime registrationDate, String description, String contractId) {
+        this.id = id;
+        this.type = type;
+        this.registrationDate = registrationDate;
+        this.description = description;
+        this.contractId = contractId;
+    }
+
+    @Id
+    @Column(name = "id", unique = true)
+    @NotNull(message = "Mandatory field")
     private String id;
+
+    @Column(name = "event_type")
     private EventType type;
+
+    @Column(name = "event_date")
     private LocalDateTime registrationDate;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "contract_id")
+    private String contractId;
+
 }
